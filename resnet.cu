725f84c7ec858149cf57d91c8dd56cb2fcf028e9
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2023-02-19 19:25:50 -0700

    changed learning rate to 1e-4

--- resnet.cu
+++ resnet.cu
@@ -2808,7 +2808,7 @@ int main(int argc, char *argv[]) {
 
 
 	// General Training Structure (holds hyperparameters and pointers to structs which have network values)
-	float LEARNING_RATE = 0.00001;
+	float LEARNING_RATE = 0.0001;
 	float MEAN_DECAY = 0.9;
 	float VAR_DECAY = 0.999;
 	float EPS = 0.00000001;
