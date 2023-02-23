01087275e702e00de9a3cce0144bef099226f8c9
Andrew Sheinberg <andrew.sheinberg@gmail.com>
2023-02-23 08:20:41 -0700

    changed lr to 1e-4 and set epochs to 40 (will be running while I'm away)

--- resnet.cu
+++ resnet.cu
@@ -3396,12 +3396,12 @@ int main(int argc, char *argv[]) {
 
 
 	// General Training Structure (holds hyperparameters and pointers to structs which have network values)
-	float LEARNING_RATE = 0.001;
+	float LEARNING_RATE = 0.0001;
 	float WEIGHT_DECAY = 0.1;
 	float MEAN_DECAY = 0.9;
 	float VAR_DECAY = 0.999;
 	float EPS = 0.0000001;
-	float N_EPOCHS = 1;
+	float N_EPOCHS = 40;
 
 	Train_ResNet * trainer = init_trainer(model, batch, BATCH_SIZE, LEARNING_RATE, WEIGHT_DECAY, MEAN_DECAY, VAR_DECAY, EPS, N_EPOCHS);
 	
